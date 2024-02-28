504be23342212ff42f497ca0739b5fc16e646991
karol-kulinowski <karol.kulinowski@gmail.com>
2024-02-28 19:01:45 +0100

    Added != operator for ComplexNumber and some docstrings

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 
 // use std::cmp::Ordering;
 
+/// Type enum (type of a number)
 enum Type {
     Real,
     Complex,
 }
 
+/// Type enum functions
 impl Type {
     fn get(&self) -> &str {
         match self {
@@ -22,12 +24,14 @@ trait Number <T, U> {
 }
 */
 
+/// Structure representing a complex number
 struct ComplexNumber {
     re: f64,
     im: f64,
     t: Type,
 }
 
+/// Buld-in functions for ComplexNumber structure
 impl ComplexNumber {
     fn new(r: f64, i: f64) -> ComplexNumber {
         ComplexNumber {
@@ -41,22 +45,30 @@ impl ComplexNumber {
     }
 }
 
+/// Comparing two complex numbers
 impl PartialEq for ComplexNumber {
     fn eq(&self, other: &Self) -> bool {
         (&self.im == &other.im) && (&self.re == &other.re) 
     }
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(&other)
+    }
 }
 
+/// Testing function
 fn main() {
     let u = ComplexNumber::new(2_f64, -1_f64);
     let w = ComplexNumber::new(2_f64, 0_f64);
     let v = ComplexNumber::new(3_f64, -1_f64);
     let z = ComplexNumber::new(2_f64, -1_f64);
+    println!("### Complex numbers ### ");
     println!("Number u={}{:+}i is {}", u.re, u.im, u.t.get());
     println!("Number w={}{:+}i is {}", w.re, w.im, w.t.get());
     println!("Number v={}{:+}i is {}", v.re, v.im, v.t.get());
     println!("Number z={}{:+}i is {}", z.re, z.im, z.t.get());
+    println!("### Logical operators tests ### ");
     println!("Are u and w equal? {}", match u == w {true => "Yes!", false => "No!"});
     println!("Are u and v equal? {}", match u == v {true => "Yes!", false => "No!"});
     println!("Are u and z equal? {}", match u == z {true => "Yes!", false => "No!"});
+    println!("Are w and v different? {}", match u != v {true => "Yes!", false => "No!"});
 }
